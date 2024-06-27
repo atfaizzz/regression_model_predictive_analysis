1f19420d309352fbbb599b15a80600c75f1cf384
Mohammad Faiz Abid <121609515+atfaizzz@users.noreply.github.com>
2024-06-28 03:13:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
 from flask import Flask, render_template, jsonify
-from titanic_dataanalysis import perform_analysis  # Import perform_analysis function from titanic_dataanalysis.py
-
+from titanic_dataanalysis import perform_analysis
 app = Flask(__name__)
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
 @app.route('/analysis')
 def analysis():
-    # Call the perform_analysis function to get analysis results
     analysis_results = perform_analysis()
-
-    # Return the analysis results as JSON
     return jsonify(analysis_results)
-
 if __name__ == '__main__':
     app.run(debug=True)
